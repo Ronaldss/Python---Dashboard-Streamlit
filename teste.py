8db22b661cfaf8e9482a3bb98de3682754256930
Ronald <ronaldcontact2019@gmail.com>
2024-08-02 06:47:39 -0300

    Exibindo dados do DataFrame a em tabela,cima do gÂráfico

--- teste.py
+++ teste.py
@@ -55,4 +55,7 @@ df2 = pd.DataFrame(
 c = alt.Chart(df2).mark_circle().encode(
         x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
         
+st.write("DADOS DO DATAFRAME")
+st.write(df2)
+st.write("GRÁFICO")
 st.write(c)
