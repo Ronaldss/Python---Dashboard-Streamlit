93299ffc057e601f3803541be5af797381cfa45e
Ronald <ronaldcontact2019@gmail.com>
2024-08-02 06:38:10 -0300

    Primeiro gráfico (Numpy, Altair)

--- teste.py
+++ teste.py
@@ -44,4 +44,15 @@ st.write('Aqui temos uma array:  ', array)
 
 
 
+'''
+TRABALHANDO COM GRÁFICOS (NUMPY, ALTAIR)
+'''
 
+df2 = pd.DataFrame(
+        np.random.randn(200, 3),
+        columns=['a', 'b', 'c'])
+
+c = alt.Chart(df2).mark_circle().encode(
+        x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+        
+st.write(c)
